869d7170b8a96d12b4bd91f9890aa138c703d07b
Minje <alswp625@naver.com>
2020-04-28 23:01:49 +0900

    Modify App.js

--- Todo_list/src/App.js
+++ Todo_list/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-function App() {
-  return (
-    <div>
-      APP
-    </div>
-  );
+import React,{Component} from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+
+class App extends Component {
+  render(){
+    return (
+      <TodoListTemplate form={<Form/>}> 
+        <TodoItemList/>
+      </TodoListTemplate>
+    );
+  }
 }
 
 export default App;
